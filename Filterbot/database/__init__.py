f39124b7ed8a35e07b72ba59240481c919450785
dumpm499-cmd <dumpm499@gmail.com>
2026-03-10 20:40:17 +0530

    Create __init__.py

---
+++ Filterbot/database/__init__.py
@@ -0,0 +1,2 @@
+from .files_db import save_file, search_files, total_files, delete_file, delete_all_files, ensure_indexes
+from .users_db import add_user, user_exists, get_all_users, total_users, delete_user
